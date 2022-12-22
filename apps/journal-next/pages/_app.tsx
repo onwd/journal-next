d8839491e9fa62aa2ff1a10cb70c14eb5c99cf04
onwd <onwdit@gmail.com>
2022-12-23 01:24:41 +0600

    fix: fix warning regarding SSRProvider

--- apps/journal-next/pages/_app.tsx
+++ apps/journal-next/pages/_app.tsx
@@ -3,17 +3,20 @@ import Head from 'next/head';
 import { NextUIProvider } from '@nextui-org/react';
 import './styles.css';
 import { lightTheme } from '../themes/light';
+import { SSRProvider } from '@react-aria/ssr';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider theme={lightTheme}>
-      <Head>
-        <title>Welcome to journal-next!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </NextUIProvider>
+    <SSRProvider>
+      <NextUIProvider theme={lightTheme}>
+        <Head>
+          <title>Welcome to journal-next!</title>
+        </Head>
+        <main className="app">
+          <Component {...pageProps} />
+        </main>
+      </NextUIProvider>
+    </SSRProvider>
   );
 }
 
